9beb05e80b6705cfdf2157f41553fb8eae542e9b
khhanhphongg <163244116+khhanhphongg@users.noreply.github.com>
2025-12-27 20:06:21 +0700

    Add files via upload

---
+++ Ex1_Ex2.py
@@ -0,0 +1,7 @@
+#Exercise 1,2:
+if __name__ == '__main__':
+   A = (3,6,1,8,6,7,3124,242,424)
+   print("Sum:",sum(A)) #Tính sum
+   print("Average:",sum(A) / len(A))  # Tính average
+   print("Max:",max(A)) #Tìm max
+   print("Min:",min(A)) #Tìm min
