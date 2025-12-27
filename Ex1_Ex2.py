7ef8866e49825bd2236275e251293b3cff483e49
khhanhphongg <163244116+khhanhphongg@users.noreply.github.com>
2025-12-27 20:32:29 +0700

    Exercise Ex1_Ex2.py

--- Ex1_Ex2.py
+++ Ex1_Ex2.py
@@ -1,6 +1,6 @@
 #Exercise 1,2:
 if __name__ == '__main__':
-   A = (3,6,1,8,6,7,3124,242,424)
+   A = [1,2,3,4,5,6,7,8,9,10]
    print("Sum:",sum(A)) #Tính sum
    print("Average:",sum(A) / len(A))  # Tính average
    print("Max:",max(A)) #Tìm max
